d3662afd768f1147ee075223ce1e9d28450c808a
root <root@debian.work>
2025-03-14 13:08:23 +0530

    Inventory->printstock increased width of actual stock column for writing multiple numbers

--- application/views/inventory/printstock.php
+++ application/views/inventory/printstock.php
@@ -9,16 +9,16 @@ $pdf->cell(190, 5, 'Closing Balance as on: '.Date('d-m-Y').' at '.$this->session
 $i=1;
 $pdf->SetFont('Arial','',12);
 $pdf->Cell(15,7,'Code',1,0,'C');
-$pdf->Cell(100,7,'Item',1,0,'C');
+$pdf->Cell(75,7,'Item',1,0,'C');
 $pdf->Cell(20,7,'Rate',1,0,'C');
 $pdf->Cell(20,7,'Cl Bal',1,0,'C');
-$pdf->Cell(30,7,'',1,1,'C');
+$pdf->Cell(55,7,'',1,1,'C');
 foreach ($stock as $st):
 $pdf->Cell(15,7,$st['id'],1,0,'C');
-$pdf->Cell(100,7,substr($st['title'],0,35),1,0,'L');
+$pdf->Cell(75,7,substr($st['title'],0,25),1,0,'L');
 $pdf->Cell(20,7,number_format($st['rate'],2,'.',''),1,0,'C');
 $pdf->Cell(20,7,$st['clbal'],1,0,'C');
-$pdf->Cell(30,7,'',1,1,'C');
+$pdf->Cell(55,7,'',1,1,'C');
 
 if ($pdf->GetY()>=270):
 $pdf->ln(5);
@@ -26,10 +26,10 @@ $pdf->Cell(190,5,'Page '.$i,0,1,'C');
 $i++;
 $pdf->AddPage();
 $pdf->Cell(15,5,'Code',1,0,'C');
-$pdf->Cell(100,5,'Item',1,0,'C');
+$pdf->Cell(75,5,'Item',1,0,'C');
 $pdf->Cell(20,5,'Rate',1,0,'C');
 $pdf->Cell(20,5,'Cl Bal',1,0,'C');
-$pdf->Cell(30,5,'',1,1,'C');
+$pdf->Cell(55,5,'',1,1,'C');
 endif;
 
 endforeach;
